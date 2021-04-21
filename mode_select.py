8fc3fc387b942681f69adc07806fd9d9dfb77522
Nyano <nayan100@hotmail.co.uk>
2021-04-21 10:09:06 +0100

    ModeSelect now creates the item_range and item_list depending on the lower and upper that is given by the user.

--- mode_select.py
+++ mode_select.py
@@ -18,11 +18,16 @@ import random
 
 
 class ModeSelect:
-    def __init__(self, mode, item_list):
+    def __init__(self, mode, lower, ending):
         self.mode = mode
         self.count = 0
         self.indexVal = 0
-        self.item_list = item_list
+        
+        self.lower = int(lower)
+        self.ending = int(ending)
+        self.item_range = list(range(self.lower, self.ending))
+        self.item_list = list(map(lambda x: self.putZeros(x), self.item_range))
+
 
     def give_values(self):
         if self.mode == 'r':
@@ -32,6 +37,12 @@ class ModeSelect:
         elif self.mode == 'n':
             self.count += 1
             return self.item_list[self.count-1]
+    
+    def putZeros(self, value):
+        if value < 10:
+            return f'0{value}'
+        else:
+            return str(value) 
 
     def change_mode(self, mode):
         self.mode = mode
@@ -41,4 +52,6 @@ class ModeSelect:
     
     def show_item_list(self):
         return self.item_list
+
+
     
